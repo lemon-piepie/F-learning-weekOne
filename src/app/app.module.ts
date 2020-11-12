42e083022e1288986cdb745c28daa6d45cae2c15
ZhaoYunqi <purplechilde@163.com>
2020-11-12 16:33:02 +0800

    [yunqi]header component with homepage and shopcart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,15 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { zh_CN } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
+import { HeaderComponent } from './components/header/header.component';
 
 registerLocaleData(zh);
 
 @NgModule({
   declarations: [
     AppComponent,
-    GoodsItemComponent
+    GoodsItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
