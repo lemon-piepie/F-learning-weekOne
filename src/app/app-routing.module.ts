065ea9ee42bf96829c5d2288b248f6a6df66971a
ZhaoYunqi <purplechilde@163.com>
2020-11-15 21:43:53 +0800

    [yunqi]add newGoodsList page and router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { GoodsListComponent } from './components/goods-list/goods-list.component';
+import { NewGoodsFormComponent } from './components/new-goods-form/new-goods-form.component';
+
+const routes: Routes = [
+  {
+    path:'home',component:GoodsListComponent
+  },
+  {
+    path:'add',component:NewGoodsFormComponent
+  },
+  {
+    path:'**',redirectTo:'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
