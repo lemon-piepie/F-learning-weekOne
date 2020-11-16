fda0c5d37df54a65ff44c67d6eabdf00b66c9a7b
ZhaoYunqi <purplechilde@163.com>
2020-11-16 09:15:15 +0800

    [yunqi]create new goodsItem after form valiation and post

--- src/app/components/new-goods-form/new-goods-form.component.ts
+++ src/app/components/new-goods-form/new-goods-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
-import { Observable, Observer } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-new-goods-form',
@@ -8,10 +8,46 @@ import { Observable, Observer } from 'rxjs';
   styleUrls: ['./new-goods-form.component.scss']
 })
 export class NewGoodsFormComponent implements OnInit {
+  validateForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  submitForm(value: { name: string; shop: string; unitPrice: number; imgUrl: string }): void {
+    for (const key in this.validateForm.controls) {
+      this.validateForm.controls[key].markAsDirty();
+      this.validateForm.controls[key].updateValueAndValidity();
+    }
+    this.createNewGoodsItem(value);
   }
 
+  resetForm(e: MouseEvent): void {
+    e.preventDefault();
+    this.validateForm.reset();
+    for (const key in this.validateForm.controls) {
+      this.validateForm.controls[key].markAsPristine();
+      this.validateForm.controls[key].updateValueAndValidity();
+    }
+  }
+
+  constructor( private fb: FormBuilder , public http: HttpClient ) {
+    this.validateForm = this.fb.group({
+      name: ['', [Validators.required]],
+      shop: ['', [Validators.required]],
+      unitPrice: ['', [Validators.required]],
+      imgUrl: ['']
+    });
+    console.log(this.validateForm.valid);
+  }
+
+  createNewGoodsItem (newGoodsItem) {
+    const httpOptions = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+    var url = "http://localhost:8080/goodsItem"; 
+    this.http.post(url,newGoodsItem,httpOptions).subscribe(response => {
+      console.log(response); 
+    });
+  }
+
+  ngOnInit(): void {
+
+  }
 }
